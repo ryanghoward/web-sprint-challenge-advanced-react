f1e69d9ae1cfa36a31c7528d835ba125403e01ef
Ryan Howard <ryanghoward@protonmail.com>
2022-01-14 11:47:56 -0800

    Plant data is received successfully

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -10,6 +10,16 @@ export default class PlantList extends Component {
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
+  componentDidMount() {
+    axios.get(`http://localhost:3333/plants`)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
